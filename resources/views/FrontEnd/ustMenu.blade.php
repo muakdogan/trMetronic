cfe544e75bb8da1a431aabec275753043b79bf72
CH4 <imac@muakdogan.cs.deu.edu.tr>
2017-10-10 11:16:41 +0300

    login form eklendi

---
+++ resources/views/FrontEnd/ustMenu.blade.php
@@ -0,0 +1,22 @@
+<header class="header">
+  <div role="navigation" class="navbar navbar-default navbar-fixed-top">
+    <div class="container">
+      <div class="navbar-header"><a href="#all" class="navbar-brand scroll-to"><!--h2>TamRekabet</h2--><img src={{asset('images_fe/logo.png')}} alt="logo" class="hidden-xs hidden-sm"><img src={{asset('images_fe/logo-small.png')}} alt="logo" class="visible-xs visible-sm"--><span class="sr-only">Go to homepage</span></a>
+        <div class="navbar-buttons">
+          <button type="button" data-toggle="collapse" data-target=".navbar-collapse" class="navbar-toggle navbar-btn">Menu<i class="pe-7s-menu"></i></button>
+        </div>
+      </div>
+      <div id="navigation" class="collapse navbar-collapse navbar-right">
+        <ul class="nav navbar-nav">
+          <li><a href="#biz" class="scroll-to">Biz Kimiz</a></li>
+          <li><a href="#nasil" class="scroll-to">Nasıl Çalışır</a></li>
+          <li><a href="#avantaj" class="scroll-to">Avantajlarımız</a></li>
+          <li><a href="#neler" class="scroll-to">Neler Var</a></li>
+          <li><a href="#iletisim" class="scroll-to">İletişim</a></li>
+        </ul>
+        <a href="{{url('/firmaKayit')}}" class="btn navbar-btn btn-ghost">ÜYE OL</a>
+        <a href="{{url('/login')}}" class="btn navbar-btn btn-ghost">ÜYE GİRİŞİ</a>
+      </div>
+    </div>
+  </div>
+</header>
