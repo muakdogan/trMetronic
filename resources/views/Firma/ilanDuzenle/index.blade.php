0e5328ed04e707dd1d43d12d88c8683ea07d0b70
Osman Kutlu <by.43.kutlu@gmail.com>
2017-10-17 16:05:14 +0300

    ilan detay sayfasinda goturu bedel butonlari onarildi

--- resources/views/Firma/ilanDuzenle/index.blade.php
+++ resources/views/Firma/ilanDuzenle/index.blade.php
@@ -150,7 +150,7 @@ Debugbar::info($ilan);
 <div class="container">
 
     @include('Firma.ilanDuzenle.ilanDuzenleM')
-    
+
 </div>
 
 <script src="http://thecodeplayer.com/uploads/js/jquery.easing.min.js" type="text/javascript"></script>
