07c2ffe047433f06ddc8c348f753f3ec57a9c2ae
Osman Kutlu <by.43.kutlu@gmail.com>
2017-10-23 07:18:14 +0300

    Profil sayfasi duzenlendi

--- public/js/ajax-crud-firmaTanitim.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-
-    //display modal form for creating new task
-    $('#btn-add-tanıtımyazısı').click(function(){
-        $('#btn-save-tanıtımyazısı').val("add");
-        $('#myModal-tanıtımyazısı').modal('show');
-    });
-});
